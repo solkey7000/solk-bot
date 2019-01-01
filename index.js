c88fb0b9c6943d051f81bf691a8ab709ba6e523d
solkey7000 <46260581+solkey7000@users.noreply.github.com>
2019-01-01 14:51:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const token = process.env.token // a garder en version heroku
 const prefix = ("?");
 const command1 = ("bonjour");
 
+//Paramètre du bot
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('Solk-bot / ?help').catch(console.error)
@@ -13,14 +14,13 @@ bot.off('not ready', function () {
     console.log("Je suis en déconnexion.")
     bot.user.setActivity('Solk-bot / OFF').catch(console.error)
 });
-
-//test
+//fin paramètre du bot
+/////////////////////////////////////
+                              //Gestion administrateur
 bot.on('guildMemberAdd', member =>{
   member.guid.channels.get('529622667402412073').send(':tada: **Bienvenue**' + member.user + ':smile: Nous sommes' + member.guild.memberCount)
   console.log('+1')
 });
-//test
-
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
         return channel.send('Bienvenue sur le serveur.' + member.displayName)
@@ -36,31 +36,36 @@ bot.on('message', function (message){
         return ban.action(message)
     }
 });
+                              //Fin gestion administrateur
+/////////////////////////////////////
+                              //Utilisation membre
+                              bot.on('message', msg => {
+
+                                  if (msg.content === "bonjour"){
+                                        msg.reply("Heureux de te revoir parmis nous.")
+                                    }
+                                  if (msg.content.match(/salut/i)) {
+                                          msg.reply('Je suis d\'accord avec toi.')
+                                  }
+                                  if (msg.content === prefix + "youtube"){
+                                      msg.channel.send("https://www.youtube.com/channel/UCE3V2oUrIlVppA8FN8H7NfA?view_as=subscriber")
+                                      console.log("Une personne a demandé pour aller sur ton site.")
+                                  }
+                                      if (msg.content === prefix + "help"){
+                                      msg.channel.send("Voici la liste des commandes ( ? + commande ):\nyoutube\n" + command1 + "\ntest")
+                                      console.log("Une personne a demandé pour aller sur ton site.")
+                                  }
+                                  if (msg.content === "."){
+                                      msg.channel.send("Sasi, Je t'aime très fort - de la part de ton Evil <3")
+                                  }
+                                     if (msg.content === "aïe"){
+                                      msg.channel.send("ME CASSE PO LE DOS")
+                                  }
+                              });
+                              //Fin utilisation membre
 
 
-bot.on('message', msg => {
-
-    if (msg.content === "bonjour"){
-          msg.reply("Heureux de te revoir parmis nous.")
-      }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "youtube"){
-        msg.channel.send("https://www.youtube.com/channel/UCE3V2oUrIlVppA8FN8H7NfA?view_as=subscriber")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
-        if (msg.content === prefix + "help"){
-        msg.channel.send("Voici la liste des commandes ( ? + commande ):\nyoutube\n" + command1 + "\ntest")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
-    if (msg.content === "."){
-        msg.channel.send("Sasi, Je t'aime très fort - de la part de ton Evil <3")
-    }
-       if (msg.content === "aïe"){
-        msg.channel.send("ME CASSE PO LE DOS")
-    }
-});
-
+                              
+//NE PAS TOUCHER
 bot.login(cfg.token); //a garder en version desktop
 bot.login(token); //a garder en version heroku
